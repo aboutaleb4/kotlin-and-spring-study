fc09dde31174be70c63cbc08d311c42f7d1311df
Ahmed <ahmedmgaboutaleb@gmail.com>
2025-05-02 08:30:20 +0300

    Experiments with extension function and the scoped function let

--- basics/src/main/kotlin/Main.kt
+++ basics/src/main/kotlin/Main.kt
@@ -1,5 +1,10 @@
 package org.example
 
+// Defining an extension Function
+fun Int.minusFive(): Int = this - 5;
+
+private var number: Int? = null;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
@@ -13,4 +18,17 @@ fun main() {
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         println("i = $i")
     }
+    
+    val int = 5;
+    
+    println(int.minusFive());
+    
+    
+    //LET
+    val x = number?.let { 
+        val number2 = it + 1
+        number2
+    } ?: 3
+    
+    println(x);
 }
