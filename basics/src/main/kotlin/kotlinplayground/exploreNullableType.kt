1b02a4198d6ee899be72ae565ba29c39156030c2
Ahmed Aboutaleb <ahmed.aboutaleb@deliveryhero.com>
2025-06-24 21:24:41 +0200

    added a comment

--- basics/src/main/kotlin/kotlinplayground/exploreNullableType.kt
+++ basics/src/main/kotlin/kotlinplayground/exploreNullableType.kt
@@ -5,6 +5,7 @@ import com.kotlinplayground.printlnInBlue
 fun exploreNullableType() {
     printlnInBlue("Explore Nullable Type")
     val nullableValue: String? = null
+//    var nonNullableValue: String = null
     println(nullableValue)
     println()
 }
