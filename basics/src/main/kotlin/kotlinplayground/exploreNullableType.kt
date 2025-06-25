5a4d3e8b7736285c5b7d74d2836f4578a5f76bbc
Ahmed Aboutaleb <ahmed.aboutaleb@deliveryhero.com>
2025-06-25 09:45:53 +0200

    exploring nullables

--- basics/src/main/kotlin/kotlinplayground/exploreNullableType.kt
+++ basics/src/main/kotlin/kotlinplayground/exploreNullableType.kt
@@ -6,6 +6,28 @@ fun exploreNullableType() {
     printlnInBlue("Explore Nullable Type")
     val nullableValue: String? = null
 //    var nonNullableValue: String = null
-    println(nullableValue)
+    var nonNullableValue: String = "string"
+    println(nullableValue?.length)
+    println(nonNullableValue.length)
+
+    val nullableValue2: String? = "passes"
+
+    val nullableValue3: String? = null
+    try {
+        printStringThatIsNotNullable(nullableValue3!!)
+    }
+    catch (e: NullPointerException) {
+        printStringThatIsNotNullable(e.javaClass.name)
+    }
+
+    nullableValue2?.let { printStringThatIsNotNullable(it) }
+    nullableValue2?.run { printStringThatIsNotNullable(this) }
+    printStringThatIsNotNullable(nullableValue2!!)
+    nullableValue3?.let { printStringThatIsNotNullable(it) }
+    nullableValue3?.run { printStringThatIsNotNullable(this) }
     println()
+}
+
+fun printStringThatIsNotNullable(string: String) {
+    println(string)
 }
