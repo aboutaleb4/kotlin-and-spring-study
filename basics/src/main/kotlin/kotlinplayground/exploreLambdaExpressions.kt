8d8c07298173ecc5b69939ed9a3b54c817f200f1
Ahmed Aboutaleb <ahmed.aboutaleb@deliveryhero.com>
2025-06-25 09:56:57 +0200

    passing a lambda as an input to a function

--- basics/src/main/kotlin/kotlinplayground/exploreLambdaExpressions.kt
+++ basics/src/main/kotlin/kotlinplayground/exploreLambdaExpressions.kt
@@ -4,5 +4,12 @@ fun exploreLambdaExpressions() {
     printlnInBlue("Explore Lambda Expressions")
     val addLambda = {x: Int, y: Int -> x + y}
     println(addLambda(1, 6))
+    calculateAndPrint(4, 5, addLambda)
     println()
+}
+
+fun calculateAndPrint(x: Int, y: Int, op: (x: Int, y: Int) -> Int): Int {
+    val output = op(x, y)
+    println(output)
+    return output
 }
