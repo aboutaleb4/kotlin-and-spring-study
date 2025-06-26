11d9ad1718fab19649d75d48763c8683d46dc71d
Ahmed Aboutaleb <ahmed.aboutaleb@deliveryhero.com>
2025-06-26 11:27:42 +0200

    first letter to lower case

--- basics/src/main/kotlin/kotlinplayground/exploreCollectionsAndFilters.kt
+++ basics/src/main/kotlin/kotlinplayground/exploreCollectionsAndFilters.kt
@@ -1,10 +1,10 @@
 package kotlinplayground
 
 fun exploreCollectionsAndFilters() {
-    FilterBasedOn { x: Int -> x % 3 == 0 }
+    filterBasedOn { x: Int -> x % 3 == 0 }
 }
 
-private fun FilterBasedOn(isDivisibleByThree: (Int) -> Boolean) {
+private fun filterBasedOn(isDivisibleByThree: (Int) -> Boolean) {
     printlnInBlue("Explore Collections And Filters")
     val collectionToExplore = listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     val filteredCollection = collectionToExplore.filter {
