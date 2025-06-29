36d02c48c79cb7d995d2bfd00d00b7220cce76fb
Ahmed Aboutaleb <ahmed.aboutaleb@deliveryhero.com>
2025-06-29 10:30:39 +0200

    filtering maps and maping more

--- basics/src/main/kotlin/kotlinplayground/exploreHashMaps.kt
+++ basics/src/main/kotlin/kotlinplayground/exploreHashMaps.kt
@@ -2,6 +2,15 @@ package kotlinplayground
 
 fun exploreHashMaps() {
     printlnInBlue("Explore HashMaps")
+    val mutableMap = mutableMapOf("key1" to 1, "key2" to 2, "key3" to 3)
+    mutableMap.forEach { key, value -> println("Key is $key, and value is $value") }
+    println(mutableMap["key1"])
+    println(mutableMap.getOrDefault("key1", 0))
+    println(mutableMap[""])
+    println(mutableMap.contains("key1"))
+    println(mutableMap.contains("key4"))
 
+    val filteredMap = mutableMap.filter { it.key != "key2" }.map { it.key.uppercase() }
+    println("Filtered Map: $filteredMap")
     println()
 }
