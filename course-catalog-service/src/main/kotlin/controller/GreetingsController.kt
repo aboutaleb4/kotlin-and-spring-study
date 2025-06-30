8d1d51755d984263044b3ff6c445ceea15388946
Ahmed Aboutaleb <ahmed.aboutaleb@deliveryhero.com>
2025-06-30 22:51:19 +0200

    New controller

---
+++ course-catalog-service/src/main/kotlin/controller/GreetingsController.kt
@@ -0,0 +1,16 @@
+package com.kotlinspring.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/greetings")
+class GreetingsController {
+    @GetMapping("/{name}")
+    fun retrieveGreetings(@PathVariable("name") name: String): String {
+
+        return "Hello $name!"
+    }
+}
