ba0bcbcd3b9af01fbf4006f5d042635f6d6ad4b7
Ahmed Aboutaleb <ahmed.aboutaleb@deliveryhero.com>
2025-07-21 09:11:58 +0200

    Comments

--- course-catalog-service/src/main/kotlin/com/example/controller/GreetingsController.kt
+++ course-catalog-service/src/main/kotlin/com/example/controller/GreetingsController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/v1/greetings")
 class GreetingsController(val greetingsService: GreetingsService) {
-    companion object: KLogging()
+    companion object: KLogging() //In order to be able to use this I had to add a dependency to build.gradle.kts
     @GetMapping("/{name}")
     fun retrieveGreetings(@PathVariable("name") name: String): String {
         logger.info { "Name is $name" }
--- course-catalog-service/src/main/kotlin/com/example/service/GreetingsService.kt
+++ course-catalog-service/src/main/kotlin/com/example/service/GreetingsService.kt
@@ -3,6 +3,9 @@ package com.example.service
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
+/**
+ * There is no interface the way we used to do it in C#.
+ */
 @Service
 class GreetingsService {
     @Value("\${message}") //Comes from application.yaml --> Good for env based variables
