0939540b7ce13fc0e9e1bcc6597f6723527a720c
Ahmed Aboutaleb <ahmed.aboutaleb@deliveryhero.com>
2025-07-26 21:22:18 +0200

    Playing more with extension functions

--- basics/src/main/kotlin/kotlinplayground/exploreExtensionFunctions.kt
+++ basics/src/main/kotlin/kotlinplayground/exploreExtensionFunctions.kt
@@ -3,9 +3,16 @@ package org.example.kotlinplayground
 // Defining an extension Function
 fun Int.minusFive(): Int = this - 5;
 
+fun String.hasMinimumLength(minLength: Int): Boolean = this.length >= minLength
+
 fun exploreExtensionFunctions() {
     printlnInBlue("Explore Extension Functions")
     val int = 5
     println(int.minusFive())
+    println()
+
+    println("Ahmed".hasMinimumLength(1))
+    println("Ahmed".hasMinimumLength(6))
+
     println()
 }
